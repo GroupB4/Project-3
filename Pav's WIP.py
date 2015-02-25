64a9009389fcfce9d3d99fe3d9ed6afa5af6fc92
Pavilion Sahota <pavilionsahota@gmail.com>
2015-02-25 10:53:48 +0000

    Pav's WIP v1.5
    it now works.
    
    the whole movement algorithm has been deleted and rewritten

--- Pav's WIP.py
+++ Pav's WIP.py
@@ -11,6 +11,10 @@ xx = 360
 yy = 240
 global xx
 global yy
+global first
+global treVal
+treVal = 0
+first = False
 global coord1
 global coord2
 global treasureList
@@ -38,7 +42,6 @@ canvas=Canvas(root,width = 650, height = 650)
 
 xpos = 0
 ypos = 0
-#print(xpos, ypos)
 canvas.create_image(xpos, ypos, image=photo)
 canvas.pack()
 
@@ -46,12 +49,13 @@ class Treasure(object):
     def __init__(self,xx,yy):
         self.treList = []
         self.xx=xx
+        self.val = 0
         self.yy=yy
         self.tre = canvas.create_rectangle(self.xx,self.yy,self.xx+50,self.yy+50,fill='white')
 
     def givecoords(self):
-        self.xx2 = xx+50
-        self.yy2 = yy+50
+        self.xx2 = self.xx+50
+        self.yy2 = self.yy+50
         return self.xx,self.yy,self.xx2,self.yy2
     
     def coordChange(self,xx,yy):
@@ -59,16 +63,20 @@ class Treasure(object):
         self.yy=yy
         canvas.coords(self.tre,self.xx,self.yy,self.xx+50,self.yy+50)
         canvas.update()
+
+    def returnCenter(self):
+        self.rx = (self.xx - self.xx2)/2
+        self.ry = (self.yy - self.yy2)/2
+        self.center = self.xx + self.rx, self.yy + self.ry
+        return self.center
+
     
 def updateTre(i,storeTre2):
-    #storeTre2 = storeTre2
-    #i = i
     treList.insert(i,storeTre2)
     return treList
 
 def nextTreasure(storeTre):
     global treList
-    #storeTre = storeTre
     treList.append(storeTre)
     print treList
     return treList
@@ -116,6 +124,7 @@ coord2.pack(side=LEFT)
 def newTre():
     global treasure1, treasure2, treasure3, treasure4, treasure5, treasure6, treasure7, treasure8, treasure9, treasure10, treasure11, treasure12
     global counting
+    global treVal
     counting+=1
     global oneShot2
     if counting == 1:
@@ -123,33 +132,43 @@ def newTre():
         treasure2 = Treasure(360,240)
     elif counting == 2:
         nextTreasure(treasure2)
+        treasure2.val = treVal
         treasure3 = Treasure(360,240)
     elif counting == 3:
         nextTreasure(treasure3)
+        treasure3.val = treVal
         treasure4 = Treasure(360,240)
     elif counting == 4:
         nextTreasure(treasure4)
+        treasure4.val = treVal
         treasure5 = Treasure(360,240)
     elif counting == 5:
         nextTreasure(treasure5)
+        treasure5.val = treVal
         treasure6 = Treasure(360,240)
     elif counting == 6:
         nextTreasure(treasure6)
+        treasure6.val = treVal
         treasure7 = Treasure(360,240)
     elif counting == 7:
         nextTreasure(treasure7)
+        treasure7.val = treVal
         treasure8 = Treasure(360,240)
     elif counting == 8:
         nextTreasure(treasure8)
+        treasure8.val = treVal
         treasure9 = Treasure(360,240)
     elif counting == 9:
         nextTreasure(treasure9)
+        treasure9.val = treVal
         treasure10 = Treasure(360,240)
     elif counting == 10:
         nextTreasure(treasure10)
+        treasure10.val = treVal
         treasure11 = Treasure(360,240)
     elif counting == 11:
         nextTreasure(treasure11)
+        treasure11.val = treVal
         treasure12 = Treasure(360,240)
     return counting
 
@@ -254,10 +273,11 @@ treasureValue.insert(2, 'Silver')
 treasureValue.insert(3, 'Gold')
 treasureValue.pack(side=LEFT)
 
-
 def varNext():
+    print treasure1.givecoords()
     global oneShot2
     global treasureList
+    global first
     oneShot2+=1
     treVal = 0
     treVar = treasureValue.curselection()
@@ -269,6 +289,10 @@ def varNext():
         treVal = 2
     elif treVar == (2,):
         treVal = 3
+    if first == False:
+        treasure1.val = treVal
+        print treasure1.val
+        first = True
     if varTreasure == 3:
         if oneShot2>2:
             print "Press start to begin"
@@ -293,13 +317,13 @@ def varNext():
         else:
             treasureList.insert(oneShot2, treVal)
             newTre()
-    return oneShot2
+    return oneShot2, treVal
    
 buttonNext=Button(root, text='Next', command=varNext)
 buttonNext.pack(side=LEFT)
 
-wishlist = Listbox(root, selectmode=MULTIPLE, width=7, height=4)
-wishlist.insert(1, 'All')
+wishlist = Listbox(root, selectmode=MULTIPLE, width=11, height=4)
+wishlist.insert(1, '-WISHLIST-')
 wishlist.insert(2, 'Bronze')
 wishlist.insert(3, 'Silver')
 wishlist.insert(4, 'Gold')
@@ -311,13 +335,10 @@ redTraffic = canvas.create_oval(3,5,3+10,5+10,fill = 'black')
 
 textWarning = canvas.create_text(600, 40, anchor=NE, text=".", fill='black')
 
-
-
 ###----------Buttons----------###
 
 #Start Robot and Timer#
 def displayUFO():
-    global treList
     Timer1=Timer(root)
     Timer1.pack()
     Timer1.Start()
@@ -325,7 +346,6 @@ def displayUFO():
     robot1.movement(canvas)
 
 Button(text="Start", cursor="trek", command=displayUFO).pack(side=LEFT, padx=20, pady=5)
-
 
 #Stop Timer#
 def END():
@@ -335,7 +355,6 @@ def END():
 
 Button(text="Stop", cursor="trek", command=END).pack(side=LEFT, padx=0, pady=5)
 
-
 #Exit Program#
 def Exit():
     if tkMessageBox.askokcancel("Exit", "Are you sure you want to leave?"):
@@ -344,16 +363,12 @@ def Exit():
 
 Button(text="Exit", cursor="trek", command=Exit).pack(side=RIGHT, padx=20, pady=5)
 
-
-
 ###----------Timer----------###
 
 toolbar = Frame(root)
 toolbar.pack(side=BOTTOM, anchor=SW, padx=10, pady=5)
 
-
 class Timer(Frame):
-
 
     #Implements timer widget#
     def __init__(self, thing=None,*toolbar):
@@ -364,13 +379,11 @@ class Timer(Frame):
         self.timestr = StringVar()
         self.timerwidget()
         
-
     #Timer widget on screen#
     def timerwidget(self):
         l = Label(self, textvariable=self.timestr)
         self.settime(self.elapsedtime)
         l.pack(padx=25, pady=14)
-
 
     #Timer format in (00:00:00) = (Minutes : Seconds : Milliseconds)#
     def settime(self, elaps):
@@ -379,13 +392,11 @@ class Timer(Frame):
         milliseconds = int((elaps - minutes*60 - seconds)*100)
         self.timestr.set('%02d:%02d:%02d'%(minutes, seconds, milliseconds))
 
-
     #Makes timer work#
     def update(self):
         self.elapsedtime = time.time() - self.start
         self.settime(self.elapsedtime)
         self.timer = self.after(50, self.update)
-
 
     #Starts timer#
     def Start(self):
@@ -393,7 +404,6 @@ class Timer(Frame):
             self.start = time.time() - self.elapsedtime
             self.update()
             self.running = 1
-
 
     #Stops timer#
     def Stop(self):
@@ -403,12 +413,20 @@ class Timer(Frame):
             self.settime(self.elapsedtime)
             self.running = 0
 
-
+def treDesc():
+    itn = random.randint(0, 5)
+        #if itn == 0:
+        #    xXx1337h4x0rzxXx = "You have discovered Mechatite, an expensive and rare material"
+        #elif itn == 1:
+        #    xXx1337h4x0rzxXx = "Upon searching the planet, you have discovered an ancient relic from the extinct natives of the planet"
+        #elif itn == 2:
+    xXx1337h4x0rzxXx = "You have discovered all the fucks i give for project 3... non!"
+    if tkMessageBox.showinfo("Treasure Found", xXx1337h4x0rzxXx):
+        pass
             
 ###----------Robot----------###
 
 class Robot(object):
-
 
     def __init__(self, x, y, treList):
         self.treList = treList
@@ -422,12 +440,12 @@ class Robot(object):
         self.rx = (self.x1 - self.x)/2
         self.ry = (self.y1 - self.y)/2
         self.tempxy =[]
+        self.sum1 = 1
+        self.sum2 = 1
+        self.valX = False
+        self.valY = False
         self.rotated = False
-        
 
-      
-
-        ##Call the method search
         self.search()
 
         robotimage_url = "http://i.imgur.com/zR2sDWw.gif"
@@ -437,66 +455,27 @@ class Robot(object):
         self.id1 = canvas.create_image(self.x,self.y, image=self.photo2)
         canvas.update()
 
-       # self.z = canvas.create_line(self.x+10,self.y+10,self.x+100,self.x+100)
-       # self.rr = self.returncenter()
-
-
-    ##The search method decides what LandMarks need to be visited and avoided
     def search(self):
-
         try:
             self.q += 1
-            print self.q
             LMcoordtemp = treList[self.q].givecoords()
-            destx = LMcoordtemp[2] - LMcoordtemp[0]
-            destx = destx/2
-            destx = destx+LMcoordtemp[0]
-            desty = LMcoordtemp[3] - LMcoordtemp[1]
-            desty = desty/2
-            desty = desty + LMcoordtemp[1]
-            self.destxy = [destx,desty]
-            self.search()
+            destxx = LMcoordtemp[0]
+            destxx+=20
+            destyy = LMcoordtemp[1]
+            destyy+=20
+            destxx2 = LMcoordtemp[2]
+            destxx2-=20
+            destyy2 = LMcoordtemp[3]
+            destyy2-=20
+            self.destxy = [destxx, destyy, destxx2, destyy2]
         except IndexError:
-            print self.q
-
-    ##Not really used finds the center on the robots oval
+            pass
+        
     def returncenter(self):
         self.center = self.x + self.rx, self.y + self.ry
         return self.center
 
-
-
-
-    ##Looks at the end of the LookAhead vector to determin whether the robot need to avoid anything or not
-    def LookAhead(self):
-            unit = self.vec1.unit()
-            #print "From Look ", unit
-            self.xAhead = self.x + unit[0]*70
-            self.yAhead = self.y + unit[1]*70
-
-            canvas.coords(self.z,self.x+10,self.y+10,self.xAhead,self.yAhead)
-
-            i=0
-            ###########x,x1,y,y1############
-            for i in range (0,self.obnum):
-                if self.LMList[i].havetresure() == False and self.xAhead >= self.listx[i] and self.xAhead <= self.listx2[i] and self.yAhead >= self.listy[i]  and self.yAhead <= self.listy2[i]:
-                    
-                    #print "Hit the sqr"
-                    self.rotated = self.vec1.rotate(90)
-                    canvas.update
-            else:
-                pass
-    ##Move from a to b
-    ##This function is to move our robot while continually call look ahead
     def movement(self,canvas):
-        dest = self.destxy
-        self.rr = self.returncenter()
-        self.vec1 = vector(self.rr,dest)
-        #canvas.create_line(self.rr,dest)
-        end = []
-        dist = self.vec1.distance()
-        i = 0
-
         global counting
         counting = 1
 
@@ -505,14 +484,47 @@ class Robot(object):
 
         global lightchange    
         lightchange = 1
+        
+        dest = self.destxy
+        self.current_coord = (self.x,self.y)
 
-        while i <= dist:
-            #print dist
-            i+= 1
-            sum1 = self.vec1.unit()
-            #self.LookAhead()
-            
-##################################################################
+        while 1==1:
+            if self.valX == True and self.valY == True:
+                self.search()
+                treDesc()
+                dest = self.destxy
+                self.valX = False
+                self.valY = False
+            else:
+                if self.valY == True:
+                    pass
+                else:
+                    if self.y > dest[1]:
+                        self.sum2 = -1
+                    elif self.y < dest[1]:
+                        self.sum2 = 1
+                    if self.y > dest[1] and self.y < dest[3]:
+                        self.valY = True
+                        self.sum2 = 0
+                    
+                if self.valX == True:
+                    pass
+                else:
+                    if self.x > dest[0]:
+                        self.sum1 = -1
+                    elif self.x < dest[0]:
+                        self.sum1 = 1
+                    if self.x > dest[0] and self.x < dest[2]:
+                        self.sum1 = 0
+                        self.valX = True
+                        
+                self.y+=self.sum2
+                self.x+=self.sum1
+                self.x1 = self.x + 20
+                self.y1 = self.y +20
+                self.current_coord = (self.x,self.y)
+                canvas.coords(self.id1, self.current_coord)
+                canvas.update()
                 
             if sleeping == 1:
                time.sleep(0.01)
@@ -522,16 +534,15 @@ class Robot(object):
                 time.sleep(1.0)
 
             counting += 1
-
         
             if counting == 1:
                 lightchange = 4
-            elif counting == 5:
+            elif counting == 15:
                 lightchange = 1
 
-            if counting == 90:
+            if counting == 200:
                 lightchange = 2
-            elif counting == 110:
+            elif counting == 235:
                 counting = 0
                 lightchange = 3
 
@@ -555,161 +566,14 @@ class Robot(object):
                 canvas.itemconfigure(redTraffic, fill = 'red')
                 sleeping = 3
                 canvas.itemconfigure(textWarning, text="Meteor shower in effect!!!", fill='white')
-                #print "Stop"
+                print "Stop"
 
             elif lightchange == 4:
                 canvas.itemconfigure(amberTraffic, fill = 'yellow')
                 canvas.itemconfigure(greenTraffic, fill = 'black')
                 canvas.itemconfigure(redTraffic, fill = 'red')
                 sleeping = 2
-            
-###############################################################################
-            
-            if self.rr == dest:
-                #print "FUFUFUFU:LASJFJSLKNFQQNOFNPIFQ"
-                dest[0] = tempxdest
-                dest[1] = tempydest
-                
-                self.vec1 = vector(self.rr,dest)
-                
-                dist = self.vec1.distance()
-                i=0
-                #dist = self.vec1.distance()
-            #print "HI", self.y
-            self.y+=sum1[1]
-            self.x+=sum1[0]
-            self.x1 = self.x + 20
-            self.y1 = self.y +20 
-            #self.current_coord = (self.x,self.y,self.x1,self.y1)
-            self.current_coord = (self.x,self.y)
 
-            
-            enddest1 = dest[0]
-            enddest2  = dest[1]
-            finalenddest = int(enddest1),int(enddest2)
-            current1 = self.x
-            current2 = self.y
-            finalcurrent = int(current1)+10,int(current2)+10
-            #print finalenddest
-            #print finalcurrent
-            #print "yy"
-  
-            ####need to create a small square
-            var1= self.destxy[0]+1
-            var2= self.destxy[0]-1
-            var3= self.destxy[1]+1
-            var4= self.destxy[1]-1
-            if self.rotated == True:
-                #print "Nothing to see here..."
-                tempxdest = dest[0]
-                tempydest = dest[1]
-                tempxy = dest
-                self.LookAhead()
-                dest[0] = self.xAhead
-                dest[1] = self.yAhead
-                self.rr= (self.x,self.y)
-                
-                self.vec2 = vector(self.rr,dest)
-                
-                end = self.vec2.returnend()
-                dist = self.vec2.distance()
-                #print dist
-                i=0
-                for x in range(0,int(dist)):
-                    sum2 = self.vec2.unit()
-                    #print"This is sum2 ", sum2
-                    self.y+=sum2[1]
-                    self.x+=sum2[0]
-                    self.x1 = self.x + 20
-                    self.y1 = self.y +20 
-                    self.current_coord = (self.x,self.y)
-                    canvas.coords(self.z,self.x+10,self.y+10,self.xAhead,self.yAhead)
-                    canvas.coords(self.id1, self.current_coord)
-                    canvas.update()
-                    time.sleep(0.01)
-                    dist = 0
-                self.q = self.q -1
-                    #dest = tempxy
-                self.rotated = False
-                self.movement(canvas)
-            
-            if finalcurrent[0] >= var2 and finalcurrent[0] <= var1 and finalcurrent[1] >= var4  and finalcurrent[1] <= var3:
-                #print "Of course it worked i never doubted it!"
-                self.search()
-                self.movement(canvas)
-            print end
-            if self.rr == end:
-                print "Or here..."
-                
-                           
-            canvas.coords(self.id1, self.current_coord)
-            canvas.update()
-            
-            
-            time.sleep(0.01)
-
-
-## class that deals with all land marks
-class LandMark:
-    def __init__(self,x,x1,y,y1,fill,tresure):
-        self.x = x
-        self.x1= x1
-        self.y = y
-        self.y1 = y1
-        self.colour = fill
-        self.visitmaybe = tresure
-        
-    
-    def CreateLM(self):
-        canvas.create_rectangle(self.x,self.x1,self.y,self.y1,fill = self.colour)        
-   # def givecoords(self):
-      #  return self.x,self.x1,self.y,self.y1
-
-    def havetresure(self):
-        if self.visitmaybe == True:
-            return True
-            
-        else:
-            return False
-
-
-
-class vector():
-    def __init__(self,list1,list2):
-        self.list2 = list2
-        self.diff = [list2[0] - list1[0], list2[1] - list1[1]]
-        
-
-    def distance(self):
-        self.a = self.diff[0]
-        self.b = self.diff[1]
-        return math.sqrt(self.a**2 + self.b**2)
-
-    def unit(self): 
-        distance = self.distance()
-        self.aunit = self.a/distance
-        self.bunit = self.b/distance
-
-        return self.aunit, self.bunit
-
-    def rotate(self,angle):
-        oldx = self.diff[0]
-        oldy = self.diff[1]
-
-        newx = oldx*math.cos(angle)-oldy*math.sin(angle)
-        newy = oldx*math.sin(angle)+oldy*math.cos(angle)
-
-        self.diff = (newx, newy)
-        return True
-    def returnend(self):
-        return self.list2
-
-
-
-
-#robot1 = Robot(20,20)
-#robot1.movement(canvas)
 
 
 root.mainloop()
-
